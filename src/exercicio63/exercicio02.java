f3fa151fba8988d9c5883e037fa9b50027ddf41a
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-17 22:42:14 -0300

    feat: calcula média de quatro notas.

---
+++ src/exercicio63/exercicio02.java
@@ -0,0 +1,34 @@
+package exercicio63;
+
+import java.util.Scanner;
+
+public class exercicio02 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float nota1, nota2, nota3, nota4, media;
+		
+		System.out.println("Digite a primeira nota:");
+		nota1 = leia.nextFloat();
+		
+		System.out.println("Digite a segunda nota:");
+		nota2 = leia.nextFloat();
+		
+		System.out.println("Digite a terceira nota:");
+		nota3 = leia.nextFloat();
+		
+		System.out.println("Digite a quarta nota:");
+		nota4 = leia.nextFloat();
+		
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.printf("A média final: %.1f%n", media);
+
+		
+		leia.close();
+
+	}
+
+}
