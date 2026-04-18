2a1f663703e47f114d5a14c8f7e5a32d11e0696e
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-17 22:10:03 -0300

    feat: adiciona leitor de dados com Scanner e implementa cálculo de novo salário

---
+++ src/exercicio63/exercicio01.java
@@ -0,0 +1,28 @@
+package exercicio63;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float salario, abono, novoSalario;
+		
+		System.out.println("Digite o salário: ");
+		salario = leia.nextFloat();
+		
+		System.out.println("Digite o abono: ");
+		abono = leia.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.printf("O salário R$ %.2f + abono R$ %.2f = R$ %.2f%n", salario, abono, novoSalario);
+
+		leia.close();
+				
+
+	}
+
+}
