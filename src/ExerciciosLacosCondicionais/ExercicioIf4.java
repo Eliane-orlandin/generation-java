397936cb4da9c0bb6751be3a3e7a86588adc02ed
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-22 15:00:24 -0300

    feat: adicionar exercício 4 de If para identificar animais por características

---
+++ src/ExerciciosLacosCondicionais/ExercicioIf4.java
@@ -0,0 +1,58 @@
+package ExerciciosLacosCondicionais;
+
+import java.util.Scanner;
+
+public class ExercicioIf4 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner (System.in);
+		
+		String caracteristica1, caracteristica2, caracteristica3;
+		
+		System.out.println("Digite a primeira característica: (vertebrado ou invertebrado)");
+		caracteristica1 = leia.next();
+		
+		System.out.println("Digite a segunda característica: (ave, mamifero, inseto ou anelideo)");
+		caracteristica2 = leia.next();
+		
+		System.out.println("Digite a terceira característica: (carnivoro, onivoro, herbivoro ou hematofago)");
+		caracteristica3 = leia.next();
+		
+		if (caracteristica1.equalsIgnoreCase("vertebrado")) {
+			if (caracteristica2.equalsIgnoreCase("ave")) {
+				if (caracteristica3.equalsIgnoreCase("carnivoro")) {
+					System.out.println("Águia");
+				} else if (caracteristica3.equalsIgnoreCase("onivoro")) {
+					System.out.println("Pomba");
+				}
+			}else if (caracteristica2.equalsIgnoreCase("mamifero")) {
+				if (caracteristica3.equalsIgnoreCase("onivoro")) {
+					System.out.println("Homem");
+				} else if (caracteristica3.equalsIgnoreCase("herbivoro")) {
+					System.out.println("Vaca");
+				}
+			}
+		} else if (caracteristica1.equalsIgnoreCase("invertebrado")) {
+			if(caracteristica2.equalsIgnoreCase("inseto")) {
+				if (caracteristica3.equalsIgnoreCase("hematofago")) {
+					System.out.println("Pulga");
+				} else if (caracteristica3.contentEquals("herbivoro")) {
+					System.out.println("Lagarta");
+				}
+			} else if (caracteristica2.equalsIgnoreCase("anelideo")) {
+				if (caracteristica3.equalsIgnoreCase("hematofago")) {
+					System.out.println("Sanguessuga");
+				} else if (caracteristica3.equalsIgnoreCase("onivoro")) {
+					System.out.println("Minhoca");
+				}
+			}
+		} else {
+			System.out.println("Combinação inválida");
+		}
+		
+		leia.close();
+		
+	}
+
+}
