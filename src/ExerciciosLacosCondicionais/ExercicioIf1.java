4dcfb1650aa56e7f51a35f72d6e55bfb47df8423
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-22 11:23:48 -0300

    feat: adicionar exercício 1 de if para comparação de soma

---
+++ src/ExerciciosLacosCondicionais/ExercicioIf1.java
@@ -0,0 +1,38 @@
+package ExerciciosLacosCondicionais;
+
+import java.util.Scanner;
+
+public class ExercicioIf1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+		
+		int valor1, valor2, valor3, soma;
+		
+		System.out.println("Digite o primeiro valor: ");
+		valor1 = leia.nextInt();
+		
+		System.out.println("Digite o primeiro valor: ");
+		valor2 = leia.nextInt();
+		
+		System.out.println("Digite o primeiro valor: ");
+		valor3 = leia.nextInt();
+		
+		soma = valor1 + valor2;
+		
+		if (soma > valor3) {
+			System.out.println("A soma de A + B é maior que C");
+		} else if (soma < valor3) {
+			System.out.println("A soma de A + B é menor que C");
+		} else {
+			System.out.println("A soma de A + B é igual que C");
+		}
+		
+		
+		
+		
+		
+		leia.close();
+	}
+
+}
