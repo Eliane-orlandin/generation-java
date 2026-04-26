b84ae517ba3bd3cf75697c94718ecdc8f378feae
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-26 11:26:31 -0300

    Exercício de Collection List para ordenação de cores

---
+++ src/ExerciciosVetoresCollections/ExercicioCollectionList.java
@@ -0,0 +1,36 @@
+package ExerciciosVetoresCollections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ExercicioCollectionList {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+
+		ArrayList <String> cores = new ArrayList<>();
+		
+		System.out.println("Digite cinco cores: (a cada cor digitada aperte o enter)");
+		for (int i = 0; i < 5; i++) {
+			//System.out.print("Digite uma cor: ");
+			cores.add(leia.nextLine());			
+			}
+		
+		System.out.println("\nListar todas as cores: ");
+		for (int i = 0; i < cores.size(); i++) {
+			System.out.println(cores.get(i));
+		}
+		
+		Collections.sort(cores);
+		System.out.println("\nOrdenar as cores: ");
+		
+		for (int i = 0; i < cores.size(); i++) {
+			System.out.println(cores .get(i));
+		}
+		
+		leia.close();
+	}
+
+}
